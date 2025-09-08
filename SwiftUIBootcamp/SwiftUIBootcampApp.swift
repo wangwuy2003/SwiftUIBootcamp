4a998d9d0b6059da6faa6cbad970a5586ee652e1
MacMini A6 <macminia6@MacMiniiiiiiiiiii.local>
2025-09-08 09:31:01 +0700

    first commit

---
+++ SwiftUIBootcamp/SwiftUIBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBootcampApp.swift
+//  SwiftUIBootcamp
+//
+//  Created by MacMini A6 on 8/9/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
