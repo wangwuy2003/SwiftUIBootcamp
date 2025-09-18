cd874f2ebbe65127713bbff36ea02f2425033f59
Nguyen Quang Huy <nquanghuy.2003@gmail.com>
2025-09-18 07:47:39 +0700

    update

---
+++ SwiftUIBootcamp/#70AnyLayout.swift
@@ -0,0 +1,46 @@
+//
+//  #70AnyLayout.swift
+//  SwiftUIBootcamp
+//
+//  Created by Apple on 17/9/25.
+//
+
+import SwiftUI
+
+struct _70AnyLayout: View {
+    @Environment(\.horizontalSizeClass) private var horizontalSizeClass
+    @Environment(\.verticalSizeClass) private var verticalSizeClass
+    
+    var body: some View {
+        VStack(spacing: 12) {
+            Text("Horizontal: \(horizontalSizeClass.debugDescription)")
+            Text("Vertical: \(verticalSizeClass .debugDescription)")
+            
+            let layout: AnyLayout = horizontalSizeClass == .compact ? AnyLayout(VStackLayout()) : AnyLayout(HStackLayout())
+            
+            layout {
+                Text("Alpha")
+                Text("Beta")
+                Text("Gamma")
+            }
+            
+//            if horizontalSizeClass == .compact {
+//                VStack {
+//                    Text("Alpha")
+//                    Text("Beta")
+//                    Text("Gamma")
+//                }
+//            } else {
+//                HStack {
+//                    Text("Alpha")
+//                    Text("Beta")
+//                    Text("Gamma")
+//                }
+//            }
+        }
+    }
+}
+
+#Preview {
+    _70AnyLayout()
+}
